d6c3e007f1faefb15cfade341baa92990d7bde5d
chensheng <272169881@qq.com>
2017-08-07 17:45:43 +0800

    make update to webviewprogress demo

--- UIWebViewProgressDemo/UIWebViewProgressDemo/WebViewController.swift
+++ UIWebViewProgressDemo/UIWebViewProgressDemo/WebViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewController: UIViewController {
     
@@ -30,40 +31,55 @@ class WebViewController: UIViewController {
         webView.loadRequest(request)
     }
     
-    var loadingCount = 0
-//    var maxLoadingCount = 0
+    var totalLoadingCount = 0
+    var finishedLoadingCount = 0
 }
 
 extension WebViewController: UIWebViewDelegate {
     
+    /* 由于 webview 每加载一个 iframe 就会调用这个回调，所以这个方法会被调用多次，尽管是一一对应的调用，但是调用顺序无法确定，所以无法确定最后一次调用
+     */
     func webViewDidStartLoad(_ webView: UIWebView) {
-        loadingCount += 1
+        totalLoadingCount += 1
 
-        if loadingCount == 1 {
+        if totalLoadingCount == 1 {
             progressView.startAnimation()
         }
     }
     
     func webViewDidFinishLoad(_ webView: UIWebView) {
-        loadingCount -= 1
+        finishedLoadingCount += 1
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        // 这个方法不可靠，总的来说，如果不用私有的 api，是无法得到准确的进度的，就连 NJK 也有 bug
+        if totalLoadingCount == finishedLoadingCount {
+            guard let state = webView.stringByEvaluatingJavaScript(from: "document.readyState") else {
+                return
+            }
             
-            
-            
-            if self.loadingCount == 0 {
-                guard let state = webView.stringByEvaluatingJavaScript(from: "document.readyState") else {
-                    return
-                }
+            if state == "complete" || state == "interactive" {
+                self.progressView.finishAnimation()
                 
-                if state == "complete" || state == "interactive" {
-                    self.progressView.finishAnimation()
-                }
+                totalLoadingCount = 0
+                finishedLoadingCount = 0
             }
         }
     }
     
     func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        progressView.finishAnimation()
+        finishedLoadingCount += 1
+        
+        // 这个方法不可靠
+        if totalLoadingCount == finishedLoadingCount {
+            guard let state = webView.stringByEvaluatingJavaScript(from: "document.readyState") else {
+                return
+            }
+            
+            if state == "complete" || state == "interactive" {
+                self.progressView.finishAnimation()
+                
+                totalLoadingCount = 0
+                finishedLoadingCount = 0
+            }
+        }
     }
 }
