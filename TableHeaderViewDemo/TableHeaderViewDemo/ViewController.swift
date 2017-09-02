d15ad49fe34c7425395ee028d922756ba9e096d1
陈胜 <272169881@qq.com>
2017-09-02 23:47:25 +0800

    make changes to TableHeaderViewDemo, still need fix

--- TableHeaderViewDemo/TableHeaderViewDemo/ViewController.swift
+++ TableHeaderViewDemo/TableHeaderViewDemo/ViewController.swift
@@ -10,15 +10,42 @@ import UIKit
 
 enum Constants {
     static let tableHeaderViewHeight: CGFloat = 200
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
+
+enum HeaderStyle: String {
+    case tableHeaderView = "tableHeaderView"
+    case customView = "customView"
 }
 
 class ViewController: UIViewController {
     
     var tableView: UITableView!
+    
     lazy var tableHeaderView: UIView = {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: Constants.tableHeaderViewHeight))
-        view.backgroundColor = .red
-        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: Constants.screenWidth, height: Constants.tableHeaderViewHeight))
+        view.layer.borderColor = UIColor.green.cgColor
+        view.layer.borderWidth = 2
+        self.addSubView(to: view)
+        return view
+    }()
+    
+    lazy var customView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: -Constants.tableHeaderViewHeight, width: Constants.screenWidth, height: Constants.tableHeaderViewHeight))
+        view.layer.borderColor = UIColor.blue.cgColor
+        view.layer.borderWidth = 2
+        self.addSubView(to: view)
+        return view
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "Lenna.png"))
+        imageView.frame = CGRect(x: 0, y: 0, width: Constants.screenWidth, height: Constants.tableHeaderViewHeight)
+        return imageView
+    }()
+    
+    func addSubView(to view: UIView) {
         let subView = UIView()
         subView.backgroundColor = .green
         subView.translatesAutoresizingMaskIntoConstraints = false
@@ -28,31 +55,76 @@ class ViewController: UIViewController {
         subView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         subView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         subView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-        return view
-    }()
-
+    }
+    
+    var headerStyle: HeaderStyle = .tableHeaderView {
+        didSet {
+            if headerStyle == .tableHeaderView {
+                useTableHeaderView()
+            } else {
+                useCustomView()
+            }
+            title = headerStyle.rawValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        title = "tableViewHeader"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "change style", style: .plain, target: self, action: #selector(changeStyle))
+        
+        // tableView
         tableView = UITableView(frame: view.bounds, style: .plain)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        // 不用 tableHeaderView 的原因是，tableHeaderView 的 Y 值无法改变
-//        tableView.tableHeaderView = tableHeaderView
-        tableView.contentInset.top = Constants.tableHeaderViewHeight
-        tableView.addSubview(tableHeaderView)
-        
         view.addSubview(tableView)
+        
+        useTableHeaderView()
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let offset = min(scrollView.contentOffset.y, -Constants.tableHeaderViewHeight)
-        
-        tableHeaderView.frame.size.height = -offset
-        tableHeaderView.frame.origin.y = offset
+        if headerStyle == .tableHeaderView {
+            let contentOffsetY = min(scrollView.contentOffset.y, 0)
+            imageView.frame.origin.y = contentOffsetY
+            imageView.frame.size.height = Constants.tableHeaderViewHeight - contentOffsetY
+        } else {
+            let contentOffsetY = min(scrollView.contentOffset.y + scrollView.contentInset.top + 64, 0)
+            print(contentOffsetY)
+            customView.frame.origin.y = -Constants.tableHeaderViewHeight
+            customView.frame.size.height = Constants.tableHeaderViewHeight - contentOffsetY
+            imageView.frame = customView.bounds
+        }
+    }
+    
+    func changeStyle() {
+        headerStyle = headerStyle == .tableHeaderView ? .customView : .tableHeaderView
+    }
+}
+
+extension ViewController {
+    // 采用 tableHeaderView + 改变其 subView 的 frame 的方式
+    func useTableHeaderView() {
+        imageView.removeFromSuperview()
+        imageView.frame = CGRect(x: 0, y: 0, width: Constants.screenWidth, height: Constants.tableHeaderViewHeight)
+        customView.removeFromSuperview()
+        tableView.contentInset.top = 0
+        tableHeaderView.addSubview(imageView)
+        tableView.tableHeaderView = tableHeaderView
+    }
+    
+    // 采用 tableView 的 subView + 设置 contentInset 的方式
+    func useCustomView() {
+        imageView.removeFromSuperview()
+        imageView.frame = CGRect(x: 0, y: 0, width: Constants.screenWidth, height: Constants.tableHeaderViewHeight)
+        tableView.tableHeaderView = nil
+        customView.addSubview(imageView)
+        tableView.addSubview(customView)
+        tableView.contentInset.top = Constants.tableHeaderViewHeight
+        tableView.contentOffset.y = -Constants.tableHeaderViewHeight
     }
 }
 
