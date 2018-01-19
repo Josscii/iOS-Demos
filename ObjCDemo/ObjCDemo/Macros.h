5efda9bedf35d0d0993fccbe1deb306c1e0f5199
chensheng <272169881@qq.com>
2018-01-19 18:38:47 +0800

    add ObjCDemo

---
+++ ObjCDemo/ObjCDemo/Macros.h
@@ -0,0 +1,44 @@
+//
+//  Macros.h
+//  ObjCDemo
+//
+//  Created by josscii on 2018/1/19.
+//  Copyright © 2018年 josscii. All rights reserved.
+//
+
+#ifndef Macros_h
+#define Macros_h
+
+#pragma mark -
+#pragma mark project convience
+
+#define APPDELEGATE ((AppDelegate *)[[UIApplication sharedApplication] delegate])
+
+#pragma mark -
+#pragma mark safe surance
+
+#define NSNull2Nil(x) ([x isEqualTo:[NSNull null]] ? nil : x)
+#define ISDictionary(x) [x isKindOfClass:[NSDictionary class]]
+#define ISArray(x) [x isKindOfClass:[NSArray class]]
+#define ISString(x) [x isKindOfClass:[NSString class]]
+
+#pragma mark -
+#pragma mark iOS Version
+
+#define IOS_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define IOS_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define IOS_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define IOS_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+#pragma mark -
+#pragma mark UIColor
+
+// example usage: UIColorFromHex(0x9daa76)
+#define UIColorFromHexWithAlpha(hexValue,a) [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0 green:((float)((hexValue & 0xFF00) >> 8))/255.0 blue:((float)(hexValue & 0xFF))/255.0 alpha:a]
+#define UIColorFromHex(hexValue)            UIColorFromHexWithAlpha(hexValue,1.0)
+#define UIColorFromRGBA(r,g,b,a)            [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+#define UIColorFromRGB(r,g,b)               UIColorFromRGBA(r,g,b,1.0)
+
+
+#endif /* Macros_h */
---
+++ ObjCDemo/ObjCDemo/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  ObjCDemo
+//
+//  Created by josscii on 2018/1/19.
+//  Copyright © 2018年 josscii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Macros.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSString *str = [NSDictionary dictionary];
+        if (ISString(str) && str.length) {
+            NSLog(@"hhhh");
+        }
+        
+    }
+    return 0;
+}
