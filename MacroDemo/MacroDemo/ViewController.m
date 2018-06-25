19c61502e312c1c52a7c27e04de88075341fd403
josscii <272169881@qq.com>
2018-06-25 23:36:55 +0800

    update macrodemo

--- MacroDemo/MacroDemo/ViewController.m
+++ MacroDemo/MacroDemo/ViewController.m
@@ -17,11 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    id a;
-    if (isValidArray(a)) {
-        
-    }
 }
 
 
